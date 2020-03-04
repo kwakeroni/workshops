55c4a11b1bd13be30f07d0d87af73aff3ff2d858
kwakeroni <kwakeroni@gmail.com>
2020-03-04 09:08:15 +0100

    Reorganized exercise & solution in separate modules

---
+++ java9/exercise/src/test/java/be/kwakeroni/workshop/java9/exercise/language/SafeVarArgs.java
@@ -0,0 +1,51 @@
+package be.kwakeroni.workshop.java9.exercise.language;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class SafeVarArgs {
+
+    /**
+     * Make the code below compile.
+     */
+    private static final String _ = "_";
+
+    /**
+     * The test below works perfectly fine, but it has some type-safety warnings.
+     * We can prove that the code is type-safe, so we want to get rid of them.
+     * Remove both warnings by adding a single annotation.
+     */
+    @Test
+    public void testSafeVarargs(){
+        List<String>[] lists = getLengths(
+                Arrays.asList("The", "Quick", "Brown", "Fox"),
+                Arrays.asList("The", "Lazy", "Dog")
+        );
+
+        Assertions.assertThat(lists[0].get(0)).isEqualTo("3");
+
+    }
+
+    private List<String>[] getLengths(List<String>... lists){
+        Object[] array = Objects.requireNonNull(lists);
+
+        for (int i=0; i<lists.length; i++){
+            List<String> lengths = new ArrayList<>();
+            List<String> strings = lists[i];
+
+            for (String string : strings){
+                lengths.add(String.valueOf(string.length()));
+            }
+
+            array[i] = lengths;
+        }
+
+        return lists;
+    }
+
+}
---
+++ java9/solution/src/test/java/be/kwakeroni/workshop/java9/solution/language/Diamond.java
@@ -0,0 +1,65 @@
+package be.kwakeroni.workshop.java9.solution.language;
+
+import java.io.Serializable;
+
+public class Diamond {
+
+    /**
+     * Creates instances of the Serializable type T
+     */
+    static abstract class Factory<T extends Serializable> {
+        public abstract T newInstance();
+    }
+
+    /**
+     * Creates instances of the type T, which is both Serializable and Comparable
+     */
+    static abstract class ComparableFactory<T extends Serializable & Comparable<?>> extends Factory<T> {
+    }
+
+    public void testInnerFactories() {
+
+        // Use the Diamond operator to reduce the boilerplate.
+        var factory = new Factory<>() {
+            @Override
+            public StringBuffer newInstance() {
+                return new StringBuffer();
+            }
+        };
+
+        // This won't work in the following case.
+        // Compare AnonymousFactory and AnonymousComparableFactory below to see why not.
+        var comparableFactory = new ComparableFactory<StringBuffer>() {
+            @Override
+            public StringBuffer newInstance() {
+                return new StringBuffer();
+            }
+        };
+    }
+
+    /*
+     * The first case is valid in Java 9, because the anonymous class
+     * can be compiled using the generic argument 'Serializable' for T
+     */
+    static class AnonymousFactory extends Factory<Serializable> {
+        @Override
+        public StringBuffer newInstance() {
+            return new StringBuffer();
+        }
+    }
+    // from bytecode: class be.kwakeroni.workshop.java9.solution.language.Diamond$1 extends be.kwakeroni.workshop.java9.solution.language.Diamond$Factory<java.io.Serializable>
+
+    /*
+     * The second case is still invalid, because there is no way to specify
+     * the generic argument 'Serializable & Comparable<?>' for T.
+     *
+     * The internal type 'Serializable & Comparable<?>' is called an intersection type.
+     * It is a non-denotable type, which means it can't be represented in source code.
+     */
+//    static class AnonymousComparableFactory extends ComparableFactory<Serializable & Comparable<?>> {
+//        @Override
+//        public Serializable & Comparable newInstance() {
+//            return "";
+//        }
+//    }
+}
